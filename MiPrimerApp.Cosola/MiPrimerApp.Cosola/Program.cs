a4e5a373df59879bd6f0baa62e744ab0d8604fe0
jse-balarezo <jse.balarezo@yavirac.edu.ec>
2021-06-15 17:26:09 -0500

    Hola Mundo

---
+++ MiPrimerApp.Cosola/MiPrimerApp.Cosola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiPrimerApp.Cosola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
